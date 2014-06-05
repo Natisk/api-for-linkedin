6d4d1db367f4481ba8a4955f00ccdf25c9086133
vitaly <realnatisk@gmail.com>
2014-06-05 18:47:46 +0300

    VCR first try

--- spec/linkedin_test.rb
+++ spec/linkedin_test.rb
@@ -1,7 +1,21 @@
 require 'rspec'
 require 'rack/test'
 require '../linked_in.rb'
+require 'vcr'
+require 'webmock/rspec'
 
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+RSpec.configure do |c|
+  c.around(:each) do |example|
+    VCR.use_cassette(example.metadata[:full_description]) do
+      example.run
+    end
+  end
+end
 
 describe LinkedIn do
   include Rack::Test::Methods
@@ -40,6 +54,12 @@ describe LinkedIn do
       it 'should return new discussions' do
         get '/retrieve_discussions'
         expect last_response.status == 200
+      end
+
+      it 'should return body' do
+        res = stub_request :get, '/callback'
+        get '/callback'
+        expect last_response == res
       end
     end
   end
