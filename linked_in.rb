4c8c973744f9119470bb64ac59816fcaa1650f9b
vitaly <realnatisk@gmail.com>
2014-06-05 13:08:52 +0300

    Net::HTTP obj in separate method

--- linked_in.rb
+++ linked_in.rb
@@ -8,6 +8,7 @@ class LinkedIn < Grape::API
   version 'v1', using: :header, vendor: 'linked_in'
   format :json
 
+
   get :callback do
     code = params[:code]
     uri = URI('https://www.linkedin.com/uas/oauth2/accessToken')
@@ -22,45 +23,50 @@ class LinkedIn < Grape::API
     redirect '/full_info'
   end
 
+  def self.fetch_response(scope)
+    uri = URI("https://api.linkedin.com/v1/#{scope}?format=json&oauth2_access_token=#{@@token}")
+    res = Net::HTTP.get_response(uri)
+    res.body
+  end
+
   # r_fullprofile
   get :full_info do
-    uri = URI("https://api.linkedin.com/v1/people/~?format=json&oauth2_access_token=#{@@token}")
-    res = Net::HTTP.get_response(uri)
-    # j_str = res.body
-    hash = JSON.parse(res.body)
+    url_part = 'people/~'
+    res = LinkedIn.fetch_response(url_part)
+    hash = JSON.parse(res)
     obj = Hashie::Mash.new(hash)
   end
 
   # r_emailaddress
   get :email do
-    uri = URI("https://api.linkedin.com/v1/people/~/email-address?format=json&oauth2_access_token=#{@@token}")
-    res = Net::HTTP.get_response(uri)
-    res.body.gsub(/[\/"]*/, '')
+    url_part = 'people/~/email-address'
+    res = LinkedIn.fetch_response(url_part)
+    res.gsub(/[\/"]*/, '')
   end
 
   # r_network
   get :connections do
-    uri = URI("https://api.linkedin.com/v1/people/~/connections?format=json&oauth2_access_token=#{@@token}")
-    res = Net::HTTP.get_response(uri)
-    str = res.body.force_encoding('UTF-8')
+    url_part = 'people/~/connections'
+    res = LinkedIn.fetch_response(url_part)
+    str = res.force_encoding('UTF-8')
     hash = JSON.parse(str)
     obj = Hashie::Mash.new(hash)
   end
 
   # rw_nus only GET
   get :retrieve_updates do
-    uri = URI("https://api.linkedin.com/v1/people/~/network/updates?format=json&oauth2_access_token=#{@@token}")
-    res = Net::HTTP.get_response(uri)
-    res.body.force_encoding('UTF-8')
+    url_part = 'people/~/network/updates'
+    res = LinkedIn.fetch_response(url_part)
+    str = res.force_encoding('UTF-8')
     hash = JSON.parse(str)
     obj = Hashie::Mash.new(hash)
   end
 
   # rw_groups only GET
   get :retrieve_discussions do
-    uri = URI("https://api.linkedin.com/v1/people/~/group-memberships?format=json&oauth2_access_token=#{@@token}")
-    res = Net::HTTP.get_response(uri)
-    hash = JSON.parse(res.body)
+    url_part = 'people/~/group-memberships'
+    res = LinkedIn.fetch_response(url_part)
+    hash = JSON.parse(res)
     obj = Hashie::Mash.new(hash)
   end
 
